8a62d44740426a2b2ec57c6b3c120a478387e5de
Iva Bozic <ivabozic23@gmail.com>
2020-08-30 18:29:46 +0100

    added sound

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const container = document.getElementById("container");
 const text = document.getElementById("text");
+const soundEl = document.getElementById("sound-toggle");
 
 const totalTime = 10000;
 const breathTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
-console.log(breathTime);
-console.log(holdTime);
+// console.log(breathTime);
+// console.log(holdTime);
 
 breathAnimation();
 
@@ -24,3 +25,21 @@ function breathAnimation() {
 }
 
 setInterval(breathAnimation, totalTime);
+
+// audio element
+
+var audioElement = new Audio("./sound/forest.wav");
+//set loop
+audioElement.loop = true;
+
+// check if audio playing, if not, on click will start it. Changes button style
+
+soundEl.addEventListener("click", (e) => {
+  if (audioElement.duration > 0 && !audioElement.paused) {
+    audioElement.pause();
+    soundEl.className = "fas fa-play-circle";
+  } else {
+    audioElement.play();
+    soundEl.className = "fas fa-pause-circle";
+  }
+});
